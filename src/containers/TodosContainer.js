c5abccef6af996bfa97d42f1f14bb6da468d01b8
RhiaKe <rhiakaulitzke@gmail.com>
2018-05-02 15:58:14 -0600

    completed sprint 3

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react'
-import TodoModel from '../models/Todo' 
+import TodoModel from '../models/Todo'
+import Todos from '../components/Todos' 
 
 class TodosContainer extends Component {
+	constructor(){
+		super()
+		this.state ={
+			todos: []
+		}
+	}
+	componentDidMount(){
+		this.fetchData()
+	}
+	fetchData(){
+		TodoModel.all().then((res) => {
+			this.setState ({
+				todos: res.data.todos,
+				todo: ''
+			})
+		})
+	}	
   render(){
-  	TodoModel.all().then( (res) => {
-  		console.log(res);
-  	})
     return (
-      <div className='TodosContainer'>
-        <h2>This is the todos container</h2>
+      <div className='todosComponent'>
+        <Todos
+        	todos={this.state.todos} />
       </div>
     )
   }
