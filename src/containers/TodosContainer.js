08732bfc5950c97d980429150f5ea26a0608bc95
RhiaKe <rhiakaulitzke@gmail.com>
2018-05-02 15:32:34 -0600

    sprint 3 mid-way

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import TodoModel from '../models/Todo' 
+
+class TodosContainer extends Component {
+  render(){
+  	TodoModel.all().then( (res) => {
+  		console.log(res);
+  	})
+    return (
+      <div className='TodosContainer'>
+        <h2>This is the todos container</h2>
+      </div>
+    )
+  }
+}
+
+export default TodosContainer
